a3ebc5e41b338dd894a9d787367bdd5b8f4993c3
Jumaron <rschurrat@gmail.com>
2022-07-03 00:21:25 +0200

    -small bug fix

--- src/OsuClient/GameField.java
+++ src/OsuClient/GameField.java
@@ -2,16 +2,12 @@ package OsuClient;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.xml.crypto.dsig.keyinfo.KeyValue;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -132,9 +128,9 @@ public class GameField extends JPanel {
     private void doDrawing(Graphics g) {
 
 
-        for (Object o : drawObject) {
+        for (int i = 0; i < drawObject.size(); i++) {
 
-            Circle circle = (Circle) o;
+            Circle circle = (Circle)drawObject.get(i);
 
             int apporachCircleSize = (int) ((circle.getTiming() - timeCounter) * 0.8 + 150 + 240);
 
